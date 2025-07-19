b678fa40f5c294591fab0a0dad7db52eaae3984e
BenAtWHOI <benjamin.foley@whoi.edu>
2025-07-18 16:33:12 -1000

    checkpoint: modify client/eslint.config.js,server/app.js,server/eslint.config.js - 16:33

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -11,7 +11,7 @@ export default [
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'commonjs',
+      sourceType: 'module',
       globals: {
         ...globals.node,
         ...globals.es2022,
