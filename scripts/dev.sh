a58ace5a591a9747f1f96480add3c22c1e054ed9
BenAtWHOI <benjamin.foley@whoi.edu>
2025-07-21 13:13:40 -1000

    fix: docker-compose -> docker compose

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,14 +2,14 @@
 echo "====================================="
 echo "== Starting development containers:"
 echo "====================================="
-docker-compose up -d
+docker compose up -d
 
 echo "====================================="
 echo "== Container status:"
 echo "====================================="
-docker-compose ps
+docker compose ps
 
 echo "====================================="
 echo "== Watching logs:"
 echo "====================================="
-docker-compose logs -f
+docker compose logs -f
--- scripts/recreate.sh
+++ scripts/recreate.sh
@@ -3,24 +3,24 @@
 echo "====================================="
 echo "== Stopping and removing containers:"
 echo "====================================="
-docker-compose down
+docker compose down
 
 echo "====================================="
 echo "== Removing images:"
 echo "====================================="
-docker-compose down --rmi all
+docker compose down --rmi all
 
 echo "====================================="
 echo "== Starting development containers:"
 echo "====================================="
-docker-compose up --build -d
+docker compose up --build -d
 
 echo "====================================="
 echo "== Container status:"
 echo "====================================="
-docker-compose ps
+docker compose ps
 
 echo "====================================="
 echo "== Watching logs:"
 echo "====================================="
-docker-compose logs -f
+docker compose logs -f
