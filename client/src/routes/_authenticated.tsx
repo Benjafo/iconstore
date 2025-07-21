21ca9952e290ffe6734bcb5bb1fc3e6da24f8b4b
BenAtWHOI <benjamin.foley@whoi.edu>
2025-07-21 10:58:17 -1000

    checkpoint: modify client/src/routes/_authenticated.tsx - 10:58

--- client/src/routes/_authenticated.tsx
+++ client/src/routes/_authenticated.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute, redirect, Outlet } from '@tanstack/react-router';
 
+interface RouteContext {
+  auth: {
+    isAuthenticated: boolean;
+  };
+}
+
 export const Route = createFileRoute('/_authenticated')({
-  beforeLoad: ({ context }) => {
+  beforeLoad: ({ context }: { context: RouteContext }) => {
     if (!context.auth.isAuthenticated) {
       throw redirect({
         to: '/auth',
