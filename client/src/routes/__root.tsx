6134bc11a2de705b40211ddbb3f7ce5232b74091
BenAtWHOI <benjamin.foley@whoi.edu>
2025-07-20 21:29:58 -1000

    checkpoint: modify client/src/routes/ - 21:29

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { createRootRoute, Outlet } from '@tanstack/router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
