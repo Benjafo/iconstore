e068e6af4dacad25884bad62c04683200eb1c1ca
BenAtWHOI <benjamin.foley@whoi.edu>
2025-07-21 18:42:45 -1000

    checkpoint: modify client/src/components/index.ts - 18:42

---
+++ client/src/components/index.ts
@@ -0,0 +1,32 @@
+// Content Display Components
+export { IconGrid } from './IconGrid';
+export { IconCard } from './IconCard';
+export { ListItem } from './ListItem';
+export { EmptyState } from './EmptyState';
+export { LoadingCard } from './LoadingCard';
+
+// Re-export types
+export type {
+  IconGridProps,
+  IconGridItem,
+} from './IconGrid';
+
+export type {
+  IconCardProps,
+  IconPackData,
+} from './IconCard';
+
+export type {
+  ListItemProps,
+  ListItemVariant,
+} from './ListItem';
+
+export type {
+  EmptyStateProps,
+  EmptyStateVariant,
+} from './EmptyState';
+
+export type {
+  LoadingCardProps,
+  LoadingCardVariant,
+} from './LoadingCard';
